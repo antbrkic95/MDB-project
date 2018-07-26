400987cad18950f85492b693e58eb6e692497178
Antonio Brkić <brkicantonio95@gmail.com>
2018-07-26 11:00:12 +0200

    Dodan je gotov program koji u novu tablicu u accessu upisuje naziv reference i pripadnu putanju (odsjeceni dio putanje) te skripta takodjer ispisuje nazive i lokacije mdb filea u svim direktorijima

---
+++ MDB/DataPath.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDB
+{
+    public class DataPath
+    {
+        public string upit;
+        public List<object[]> getData(string path)
+        {
+            List<object[]> lista = new List<object[]>();
+            OleDbDataReader reader;
+            string connectString = @"Provider = Microsoft.Jet.OLEDB.4.0; Data Source =" + "" + path;
+            OleDbConnection cn = new OleDbConnection(connectString);
+            cn.Open();
+            try
+            {
+                upit = "select reference from data;"; 
+                OleDbCommand cmd = new OleDbCommand(upit, cn);
+                reader = cmd.ExecuteReader();   
+                while(reader.Read())
+                {
+                    object[] row = new object[1];
+                    reader.GetValues(row);
+                    lista.Add(row);
+                }
+            }
+            catch { }
+            cn.Close();
+            return lista;
+        }
+         
+        public string[] listAllMdbFiles()
+        {
+            string file = "*.mdb";
+            string[] filePaths = Directory.GetFileSystemEntries(@"C:\PROJEKT mdb\SKF", file, SearchOption.AllDirectories);
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                Console.WriteLine(filePaths[i]);              
+            }
+            return filePaths;
+        }
+        public void Sign() {
+            string[] pathsMDBFiles = listAllMdbFiles();
+            string connection = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\PROJEKT mdb\mdb_path\path_reference.mdb";
+            OleDbConnection cn = new OleDbConnection(connection);
+            cn.Open();
+            for (int i = 0; i < pathsMDBFiles.Length; i++)
+            {
+                List<object[]> data = getData(pathsMDBFiles[i]);
+                pathsMDBFiles[i] = pathsMDBFiles[i].Replace(@"C:\PROJEKT mdb\SKF\", "");
+                foreach (var item in data)
+                    {
+                    string upit = "insert into path_reference (reference,mdb_path) values('" + item[0].ToString() + "','" + pathsMDBFiles[i] + "')";
+                    //string upit = "delete * from path_reference";
+                    OleDbCommand cmd = new OleDbCommand(upit, cn);
+                        OleDbDataReader reader = cmd.ExecuteReader();
+                    }
+            }
+                cn.Close();
+            }
+        }
+    }
+
---
+++ MDB/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MDB
+{
+    class Program
+    {
+        string upit;
+        static void Main(string[] args)
+        {
+            DataPath dp = new DataPath();
+            
+
+            dp.Sign();
+            Console.WriteLine("Press enter to continue");
+            Console.Read();
+        }
+       
+       
+        //vraca putanju od svih koja je odsjecena i zapisuje u txt datoteku
+   
+       
+    }
+}
+
